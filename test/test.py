a333e15894efd51f3865e6ea98b4fa21278bd675
bramuel <jorammanoah1@gmail.com>
2021-07-02 10:31:11 -0700

    Removed self

--- test/test.py
+++ test/test.py
@@ -5,48 +5,48 @@ input_phone = "+254799980846"
 
 
 #Check if answer == input; return True
-def test_equal(self):
+def test_equal():
     result = compare_nums.compare_nums(2,2)
-    self.assertEqual(result,True)
+    assertEqual(result,True)
 
 
 # Check if input != answer
-def test_not_equal(self):
+def test_not_equal():
     result = compare_nums.compare_nums(1,2)
-    self.assertEqual(result, None)
+    assertEqual(result, None)
 
 
 #Check if input > answer
-def test_greater_than(self):
+def test_greater_than():
     result = compare_nums.is_greater_than(4,3)
-    self.assertTrue(result,False)
+    assertTrue(result,False)
 
 
-def test_lesser_than(self):
+def test_lesser_than():
     result = compare_nums.is_greater_than(3,4)
-    self.assertFalse(result, False)
+    assertFalse(result, False)
 
 
 # Check decrement func
-def test_decrement(self):
+def test_decrement():
     result = compare_nums.decrement(5)
-    self.assertEqual(4,4)
+    assertEqual(4,4)
 
 
-def test_input_pass(self):
+def test_input_pass():
     assert(int(input("Input greater than or equal to 0: ")) >= 0)
 
 
-def test_input_fail(self):
+def test_input_fail():
     assert(int(input("Input lesser than 0: ")) < 0)
 
 
 #Check phone number length
-def test_phone_number_length(self):
+def test_phone_number_length():
     assert(len(input_phone) == 13)
 
 #Check if phone number digits
-def test_phone_number_digits(self):
+def test_phone_number_digits():
     phone = "254799980846"
     assert(input_phone[1:]==phone)
     assert(phone.isdigit() == True)
