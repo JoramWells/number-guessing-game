a3963906eb59e004042147bb485099e2ac58e1ad
bramuel <jorammanoah1@gmail.com>
2021-07-07 05:57:06 +0300

    Changed color

--- tkinter_app.py
+++ tkinter_app.py
@@ -22,7 +22,7 @@ TARGET = random.randint(0, 1000)
 count = 10
 
 
-def update_result(text):
+def update(text):
     result.configure(text=text)
 
 # Create a new game
@@ -30,21 +30,21 @@ def new_game():
     guess_button.config(state='normal')
     global TARGET
     TARGET = random.randint(0, 1000)
-    update_result(text="Guess a number between 1 and 1000")
+    update(text="Guess a number between 1 and 1000")
 
 def compare_size(rand,guess,count):
     if(rand > guess):
         count -= 1
-        hint = "Value is less by: ", rand - guess, " total score " , count
-        return hint
+        return ("Value is less by: {}".format(rand - guess), " total score {}".format(count))
 
     elif(rand < guess):
-        return "Value more by: ", guess - rand, " total score ", count
+        count -= 1
+        return ("Value more by: ", guess - rand, " total score ", count)
 
     else:
         guess_button.configure(state='disabled')
         number_form.configure(state='disabled')
-        send_button.configure(state='normal')
+        send_button.config(state='normal')
         return "Total score is ", count, " please enter your number in the field above"
 
 
@@ -52,11 +52,11 @@ def get_phone_number():
     phone_number = phone_form.get()
     if(len(phone_number) != 13):
         result =  "Please enter a valid phone number: "
-        update_result(result)
+        update(result)
 
     if(phone_number[:4] != "+254"):
         result = "Country code begins with +254: "
-        update_result(result)
+        update(result)
 
 
     send_msg(phone_number)
@@ -65,8 +65,11 @@ def send_msg(phone_number):
     hour = int(datetime.datetime.now().hour)
     minute = int(datetime.datetime.now().minute)
     message = "You scored  out of 10"
-    update_result(message)
-    pywhatkit.sendwhatmsg(phone_number , message , hour , minute+2)
+    update(message)
+    try:
+        pywhatkit.sendwhatmsg(phone_number , message , hour , minute+2)
+    except ValueError:
+        print("wtr")
 
 
         
@@ -80,22 +83,20 @@ def play_game():
         try:
             choice = int(number_form.get())
             result = compare_size(TARGET,choice,count)    
-            update_result(result)
+            update(result)
         except ValueError:
             result = "Invalid input"
-            update_result(result)
+            update(result)
 
 
-# Heading of our game
-title = tk.Label(window,text="Guessing Game",font=("Arial",24),fg="#fffcbd",bg="#36454f")
+title = tk.Label(window,text="Amazing game",font=("Arial",18),fg="#fffcbd",bg="#36454f")
 
-# Result and hints of our game
-result = tk.Label(window, text="Click on Play to start a new game", font=("Arial", 12, "normal", "italic"),fg = "White", bg="#065569", justify=tk.LEFT)
+result = tk.Label(window, text="Click START to begin a new game", font=("Arial", 12, "normal", "italic"),fg = "White", bg="#36454f", justify=tk.LEFT)
+phone_lable = tk.Label(window, text="Enter phone number", font=("Arial", 11, "normal", "italic"),fg = "Whitesmoke", bg="#36454f", justify=tk.LEFT)
 
-# Play Button
+
 play_button = tk.Button(window, text="START", font=("Arial", 12, "bold"), fg = "Black", bg="#00B25D", command=new_game)
 
-# Guess Button
 guess_button = tk.Button(window,text="Guess",font=("Arial",13), state='disabled', fg="#13d675",bg="#282C35", command=play_game)
 send_button = tk.Button(window,text="SEND",font=("Arial",13), state='disabled', fg="gray",bg="#0088D8", command=get_phone_number)
 
@@ -114,19 +115,21 @@ phone_form = tk.Entry(window,font=("Arial",11),textvariable=phone_number)
 
 # Place the labels
 
-title.place(x=250, y=50)
+title.place(x=290, y=50)
 result.place(x=250, y=250)
 
 # Place the buttons
 exit_button.place(x=300,y=320)
 guess_button.place(x=450, y=145) 
-send_button.place(x=450, y=190) 
+send_button.place(x=450, y=210) 
 
 play_button.place(x=325, y=90)
 
 # Place the entry field
 number_form.place(x=180, y=150)
-phone_form.place(x=180, y=200)
+phone_form.place(x=180, y=220)
+phone_lable.place(x=180, y=195)
+
 
 
 # Start the window
